5d7311d92ce2e2d600dbbe6e979fd4b96260d37f
nghia.vuong <nghia.vuong@vn.defide.co.jp>
2021-06-09 17:01:56 +0700

    init project

---
+++ AndroidLiveDataViewModel/app/src/main/java/com/nghia/androidlivedataviewmodel/MainActivityLifecycle.kt
@@ -0,0 +1,9 @@
+package com.nghia.androidlivedataviewmodel
+
+import androidx.lifecycle.LifecycleObserver
+
+/**
+ * Created by nghia.vuong on 09,June,2021
+ */
+class MainActivityLifecycle : LifecycleObserver {
+}
---
+++ AndroidLiveDataViewModel/app/src/main/java/com/nghia/androidlivedataviewmodel/MainActivityViewModel.kt
@@ -0,0 +1,12 @@
+package com.nghia.androidlivedataviewmodel
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by nghia.vuong on 09,June,2021
+ */
+class MainActivityViewModel : ViewModel() {
+
+}
